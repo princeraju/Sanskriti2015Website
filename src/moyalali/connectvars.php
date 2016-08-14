8377e5fa3c0e3ecad61dc53b0000c27615f79b6d
Prince Raju <princeraju7777@gmail.com>
2016-08-14 15:09:51 +0530

    Cleaning code

--- src/moyalali/connectvars.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST2325','localhost');
-define('DB_USER2325','root');
-define('DB_PASSWORD2325','');
-define('DB_NAME2325','sanskriti');
-?>
--- src/moyalali/connectvars1.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST2325','localhost');
-define('DB_USER2325','u427724035_root');
-define('DB_PASSWORD2325','ivon18th');
-define('DB_NAME2325','u427724035_main');
-?>
